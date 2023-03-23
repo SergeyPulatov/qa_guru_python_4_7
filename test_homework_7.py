e5bcce205f9e08be2bd13cedd50621c9e5e0955b
sergey.pulatov <sergey.pulatov@gs-labs.ru>
2023-03-24 02:08:27 +0300

    Homework 7 zip close added

--- test_homework_7.py
+++ test_homework_7.py
@@ -13,34 +13,31 @@ def test_making_zip():
     new_zip.write('file_example_XLS_10.xls')
     new_zip.write('docs-pytest-org-en-latest.pdf')
     new_zip.close()
-    shutil.move(os.getcwd() + '/files.zip',  os.getcwd() + '/resources' + '/files.zip')
+    shutil.move(os.getcwd() + '/files.zip', os.getcwd() + '/resources' + '/files.zip')
     yield
     os.remove(os.getcwd() + '/resources' + '/files.zip')
+
 
 def test_csv(test_making_zip):
     with zipfile.ZipFile(os.getcwd() + '/resources' + '/files.zip', 'r') as my_zip:
         csv_file = my_zip.open('username.csv')
         check_csv_file_length_lines = len(csv_file.readlines())
         assert check_csv_file_length_lines == 6
+        my_zip.close()
+
+
 def test_xls(test_making_zip):
     with zipfile.ZipFile(os.getcwd() + '/resources' + '/files.zip', 'r') as my_zip:
         xlx_file = my_zip.open('file_example_XLS_10.xls')
         xlx_read = pd.read_excel(xlx_file)
         check_first_name = xlx_read.at[4, 'First Name']
         assert check_first_name == 'Nereida'
+        my_zip.close()
+
 
 def test_pdf(test_making_zip):
     with zipfile.ZipFile(os.getcwd() + '/resources' + '/files.zip', 'r') as my_zip:
         pdf_file = PdfReader(my_zip.open('docs-pytest-org-en-latest.pdf'))
         pdf_check_page = pdf_file.pages[5].extract_text()
         assert pdf_check_page == 'pytest Documentation, Release 0.1\n2 CONTENTS'
-
-
-
-
-
-
-
-
-
-
+        my_zip.close()
